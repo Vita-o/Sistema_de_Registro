83fb68acc9a40de7c07a54ead88cd4ebdf3de65a
VictorBarbosa <victorcavalcantesb@gmail.com>
2025-05-08 09:17:24 -0300

    Criando a Tela

---
+++ Tela.py
@@ -0,0 +1,26 @@
+#importando dependencias do Tkinter
+from tkinter.ttk import *
+from tkinter import*
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog as fd
+
+# importando pillow
+from PIL import ImageTk, Image
+
+# tk calendar
+from tkcalendar import Calendar, DateEntry
+from datetime import date
+
+# cores
+co0 = "#2e2d2b"  # Preta
+co1 = "#feffff"  # Branca   
+co2 = "#e5e5e5"  # grey
+co3 = "#00a095"  # Verde
+co4 = "#403d3d"   # letra
+co6 = "#003452"   # azul
+co7 = "#ef5350"   # vermelha
+
+co6 = "#146C94"   # azul
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # + verde
