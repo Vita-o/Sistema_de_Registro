f086f87e9aff54cc90440be3d7a60d5ac59e7c03
VictorBarbosa <victorcavalcantesb@gmail.com>
2025-05-08 15:29:32 -0300

    Criando tela e add Logos

--- Tela.py
+++ Tela.py
@@ -15,7 +15,7 @@ from datetime import date
 # cores
 co0 = "#2e2d2b"  # Preta
 co1 = "#feffff"  # Branca   
-co2 = "#e5e5e5"  # grey
+co1 = "#e5e5e5"  # grey
 co3 = "#00a095"  # Verde
 co4 = "#403d3d"   # letra
 co6 = "#003452"   # azul
@@ -37,7 +37,25 @@ style.theme_use("clam")
 
 # Criando Frames
 frame_logo = Frame(janela, width=850, height=52, bg=co6)
-frame_logo.grid(row=0, column=0, padx=0, sticky=NSEW, columnspan=5)
+frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW, columnspan=5)
+
+frame_botoes = Frame(janela, width=100, height=200, bg=co1, relief=RAISED)
+frame_botoes.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+frame_details = Frame(janela, width=800, height=100, bg=co1, relief=SOLID)
+frame_details.grid(row=1, column=1, pady=1, padx=10, sticky=NSEW)
+
+frame_tabela = Frame(janela, width=800, height=100, bg=co1, relief=SOLID)
+frame_tabela.grid(row=3, column=0, pady=0, padx=10, sticky=NSEW,columnspan=5)
+
+
+# Frame Logo
+
+
+
+
+
+
 
 
 
