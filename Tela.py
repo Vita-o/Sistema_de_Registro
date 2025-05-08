27dbdfd1c3f8029f3a1b5b2981bf358a51e5df79
VictorBarbosa <victorcavalcantesb@gmail.com>
2025-05-08 16:40:16 -0300

    Criando Campos

--- Tela.py
+++ Tela.py
@@ -25,7 +25,7 @@ co6 = "#146C94"   # azul
 co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # + verde
 
-# Criando Janela
+#================================== Criando Janela ==================================
 janela = Tk()
 janela.title("")
 janela.geometry("810x535")
@@ -35,7 +35,7 @@ janela.resizable(width=FALSE, height=FALSE)
 style = Style(janela)
 style.theme_use("clam")
 
-# Criando Frames
+# ==================================Criando Frames ==================================
 frame_logo = Frame(janela, width=850, height=52, bg=co6)
 frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW, columnspan=5)
 
@@ -49,11 +49,36 @@ frame_tabela = Frame(janela, width=800, height=100, bg=co1, relief=SOLID)
 frame_tabela.grid(row=3, column=0, pady=0, padx=10, sticky=NSEW,columnspan=5)
 
 
-# Frame Logo
+#================================== Frame Logo ==================================
+global imagem, imagem_string, l_imagem
 
+app_lg = Image.open('Icones/Logo.png')
+app_lg = app_lg.resize((50,50))
+app_lg = ImageTk.PhotoImage(app_lg)
+app_logo = Label(frame_logo, image=app_lg, text=" Sistema de Registro de Aluno ", width=850, compound=LEFT, anchor=NW, font=('Verdana 15'), bg=co6, fg=co1)
+app_logo.place(x=5, y=0)
 
+# ==================================Campos de Entrada==================================
+l_nome = Label(frame_details, text="Nome *", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_nome.place(x=4, y=10)
+e_nome = Entry(frame_details, width=30, justify='left', relief='solid')
+e_nome.place(x=7, y=40)
 
+l_email = Label(frame_details, text="Email *", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_email.place(x=4, y=70)
+e_email = Entry(frame_details, width=30, justify='left', relief='solid')
+e_email.place(x=7, y=100)
 
+l_tel = Label(frame_details, text="Telefone *", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_tel.place(x=4, y=130)
+e_tel = Entry(frame_details, width=7, justify='left', relief='solid')
+e_tel.place(x=7, y=160)
+
+l_sexo = Label(frame_details, text="Sexo *", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_sexo.place(x=127, y=130)
+c_sexo = ttk.Combobox(frame_details, width=7, font=('Ivy 8'),justify='center')
+c_sexo['values'] = ('MASCULINO', 'FEMININO')
+c_sexo.place(x=130, y=160)
 
 
 
